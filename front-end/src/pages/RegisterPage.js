92f174072904e402b89722f5d02c0373014af09a
Julio Cezar <jctaraujo@hotmail.com>
2020-07-27 17:26:59 -0300

    Finalizado register page

--- front-end/src/pages/RegisterPage.js
+++ front-end/src/pages/RegisterPage.js
@@ -1,25 +1,57 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const textInputs = (type, text, value, setValue) => (
+import '../styles/RegisterPage.css';
+
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const textInputs = (type, text, value, setValue, testId) => (
   <label htmlFor={text}>
     {text}
-    <input type={type} name={text} value={value} onChange={(e) => setValue(e.target.value)} />
+    <input type={type}
+      name={text}
+      value={value}
+      data-testid={testId}
+      onChange={(e) => setValue(e.target.value)} />
   </label>
 );
 
-const checkBox = (type, text, checked, setValue) => (
+const checkBox = (type, text, checked, setValue, testId) => (
   <label htmlFor={text}>
     {text}
-    <input type={type} name={text} checked={checked} onChange={(e) => setValue(e.target.checked)} />
+    <input
+      type={type}
+      name={text}
+      checked={checked}
+      data-testid={testId}
+      onChange={(e) => setValue(e.target.checked)} />
   </label>
 );
 
 const requestRegister = async (name, email, password, role) => {
-  console.log(name, email, password, role);
   const resp = await axios.post('http://localhost:3001/register', { name, email, password, role })
     .catch((err) => console.log(err));
   console.log(resp);
+};
+
+const verifyValues = (name, password, email) => {
+  if (!name || name.length <= 12 || typeof name !== 'string') {
+    return { error: 'name' }
+  };
+  if (!password || password.length <= 6) {
+    return { error: 'pass' }
+  };
+  if (!email.match(emailRegex)) {
+    return { error: 'email' }
+  }
+  return true;
+};
+
+const clearFields = (setNameData, setEmailData, setPassData, setSellerData) => {
+  setNameData('');
+  setPassData('');
+  setEmailData('');
+  setSellerData(false);
 };
 
 const RegisterPage = () => {
@@ -29,22 +61,38 @@ const RegisterPage = () => {
   const [sellerData, setSellerData] = useState(false);
 
   const handleSubmit = (event) => {
-    setNameData('');
-    setPassData('');
-    setEmailData('');
-    setSellerData(false);
+    const isValid = verifyValues(nameData, passData, emailData);
+    if (isValid.error === 'name') {
+      alert('O nome deve possuir 12 caracteres e sem caracteres especiais');
+      setNameData('');
+      return event.preventDefault();
+    }
+    if (isValid.error === 'pass') {
+      alert('A senha deve conter ao menos 6 números');
+      setPassData('');
+      return event.preventDefault();
+    }
+    if (isValid.error === 'email') {
+      alert('Formato de Email invalido');
+      setEmailData('');
+      return event.preventDefault();
+    }
+    clearFields(setNameData, setEmailData, setPassData, setSellerData);
     requestRegister(nameData, emailData, passData, sellerData);
     event.preventDefault();
   };
 
+  const disabled = verifyValues(nameData, passData, emailData);
+  console.log(disabled);
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        {textInputs('text', 'Nome', nameData, setNameData)}
-        {textInputs('email', 'Email', emailData, setEmailData)}
-        {textInputs('password', 'Password', passData, setPassData)}
-        {checkBox('checkbox', 'Quero Vender', sellerData, setSellerData)}
-        <input type="submit" value="Cadastrar" />
+        {textInputs('text', 'Nome', nameData, setNameData, "signup-name")}
+        {textInputs('text', 'Email', emailData, setEmailData, "signup-email")}
+        {textInputs('number', 'Password', passData, setPassData, "signup-password")}
+        {checkBox('checkbox', 'Quero Vender', sellerData, setSellerData, "signup-seller")}
+        <input type="submit" value="Cadastrar" data-testid="signup-btn" disabled={disabled.error} />
       </form>
     </div>
   );
