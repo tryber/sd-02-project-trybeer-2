2eb7f548db851a25afdd2c5b60ff0f2d16b4627d
Mateus Talles <mateustalles@gmail.com>
2020-07-27 19:11:45 -0300

    Wrong redirection removed from App and Style added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react';
-import { Router, Switch, Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminHome from './pages/admin/Home'
 import ClientProducts from './pages/client/Products';
@@ -7,7 +7,6 @@ import history from './services/history';
 import './App.css';
 
 function App() {
-  useEffect(() => <Redirect to="/login" />);
   return (
     <div className="App">
       <Router history={history}>
