74c5fb2e4086e405b66a3404c057219aaa050bb8
Mateus Talles <mateustalles@gmail.com>
2020-07-27 18:31:04 -0300

    Added Login automated redirection

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Router, Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import AdminHome from './pages/admin/Home'
 import ClientProducts from './pages/client/Products';
@@ -7,13 +7,15 @@ import history from './services/history';
 import './App.css';
 
 function App() {
+  useEffect(() => <Redirect to="/login" />);
   return (
     <div className="App">
       <Router history={history}>
         <Switch>
           <Route path="/admin/home" component={AdminHome} />
           <Route path="/client/products" component={ClientProducts} />
-          <Route path="/" component={Login} />
+          <Route path="/login" component={Login} />
+          <Route path="/" render={() => history.push('/login')} />
         </Switch>
       </Router>
     </div>
