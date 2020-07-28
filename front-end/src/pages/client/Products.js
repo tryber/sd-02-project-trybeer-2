ce6dbbfc47ef4a432c539300e8facf3f94bc23a1
Mateus Talles <mateustalles@gmail.com>
2020-07-28 18:38:14 -0300

    get request

--- front-end/src/pages/client/Products.js
+++ front-end/src/pages/client/Products.js
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const productsRequest = async () => {
+  const { token } = JSON.parse(localStorage.getItem('user'));
+    const productData = await axios({
+    baseURL: `http://localhost:3001/products`,
+    method: 'get',
+    headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': token }
+  })
+  .catch(({ response: { status, data: { error: { message }}} }) => console.log(`Error: ${status}. ${message}`));
+
+  return console.log(productData);
+};
 
 export default function ClientProducts () {
+  const [productData, setProductData] = useState([])
+
+  useEffect(() => {
+    const productsRequest = async () => {
+      const { token } = JSON.parse(localStorage.getItem('user'));
+        const productData = await axios({
+        baseURL: `http://localhost:3001/products`,
+        method: 'get',
+        headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': token }
+      })
+      .catch(({ response: { status, data: { error: { message }}} }) => console.log(`Error: ${status}. ${message}`));
+
+      return console.log(productData);
+    };
+    productsRequest()
+    }, [])
   return <div>Produtos Cliente</div>
 }
