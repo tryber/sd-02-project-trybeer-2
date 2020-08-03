6d5f4df47a64341ef74b8541963e6bb8034f0edc
Mateus Talles <mateustalles@gmail.com>
2020-08-03 16:39:49 -0300

    cart behavior normalized

--- front-end/src/pages/client/Checkout.js
+++ front-end/src/pages/client/Checkout.js
@@ -2,11 +2,34 @@ import React, { useEffect, useState } from 'react';
 import '../../styles/Checkout.css';
 
 const formatPrice = (totalPrice) => new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(totalPrice);
+
+
 export default function Checkout() {
   const [cartData, setCartData] = useState([]);
 
+  const removeItem = (itemId) => {
+    const actualCart = JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : null;
+    if(actualCart !== null) {
+      const newCart = actualCart.filter(({ id }) => id !== itemId)
+      console.log(String(newCart));
+      if (String(newCart) === '') {
+        localStorage.removeItem('cart');
+        return setCartData(null)
+      }
+      localStorage.removeItem('cart');
+      localStorage.setItem('cart', JSON.stringify(newCart));
+      return setCartData(newCart);
+    }
+    return setCartData(null);
+  }
+
   useEffect(() => {
     const getAddressInfoFromLocalStorage = () => JSON.parse(localStorage.getItem('cart')) ? JSON.parse(localStorage.getItem('cart')) : null;
+    const actualCart = getAddressInfoFromLocalStorage();
+    if(!actualCart || actualCart.length === 0) {
+      setCartData(null);
+      localStorage.removeItem('cart');
+    }
     setCartData(getAddressInfoFromLocalStorage());
   }, [setCartData]);
 
@@ -18,13 +41,14 @@ export default function Checkout() {
         </div>
         <div className="products-list-container">
           <ul>
-            {cartData ? cartData.map(({ itemQty, name, price, totalValue }, index) => (
+            {cartData ? cartData.map(({ id, itemQty, name, price, totalValue }, index) => (
               <li key={name} className="product-item">
                 <span data-testid={`${index}-product-qtd-input`} className="span-itemQty">{itemQty}</span>
                 <span data-testid={`${index}-product-name`}>{`- ${name}`}</span>
                 <span className="spacing-span"></span>
                 <span className="span-total-product-price" data-testid={`${index}-product-total-value`}>{`${formatPrice(totalValue)}`}</span>
                 <span className="span-unit-price">{`(${formatPrice(price)} un)`}</span>
+                <button className="item-removal-button" onClick={() => removeItem(id)}>X</button>
               </li>
             )) : <h2>Não há produtos no carrinho</h2>}
           </ul>
