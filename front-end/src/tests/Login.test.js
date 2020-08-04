5998e7c9d010b8b2ff38f970c393bb6e0782ef93
eddyeboy27 <lipe0503@gmail.com>
2020-08-04 19:39:38 -0300

    Testado funcionalidades dos elementos HTML

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -5,7 +5,7 @@ import Provider from '../context/TrybeerContext';
 import Login from '../pages/Login';
 
 describe('Testing Login Page', () => {
-  test('Testing if HTML elements appears', async () => {
+  test('Testing if HTML elements appears', () => {
     const { queryByTestId } = render(
       <Login />
     );
@@ -17,6 +17,37 @@ describe('Testing Login Page', () => {
     expect(signInButton).toBeInTheDocument();
     const noAccountButton = queryByTestId('no-account-btn');
     expect(noAccountButton).toBeInTheDocument();
+  });
 
-  })
-})
+  test('Testing funcionality of HTML elements', () => {
+    const { queryByTestId } = render(
+      <Login />
+    );
+    
+    const emailInput = queryByTestId('email-input');
+    const passInput = queryByTestId('password-input');
+    const signInButton = queryByTestId('signin-btn');
+
+    expect(emailInput.value).toBe('');
+    expect(passInput.value).toBe('');
+    expect(signInButton.disabled).toBeTruthy();
+    fireEvent.change(emailInput, { target: { value: 'trybe@.email.com' } });
+    expect(emailInput.value).toBe('trybe@.email.com');
+    expect(signInButton.disabled).toBeTruthy();
+    fireEvent.change(passInput, { target: { value: '12345' } });
+    expect(passInput.value).toBe('12345');
+    expect(signInButton.disabled).toBeTruthy();
+    fireEvent.change(emailInput, { target: { value: 'trybe@email.com' } });
+    fireEvent.change(passInput, { target: { value: '123456' } });
+    expect(emailInput.value).toBe('trybe@email.com');
+    expect(passInput.value).toBe('123456');
+    expect(signInButton.disabled).toBeFalsy();
+  });
+
+  test('Testing new user button', async () => {
+    const { queryByTestId } = render(
+      <Login />
+    );
+    
+  });
+});
