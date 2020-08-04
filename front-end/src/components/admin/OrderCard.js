c91d9fab8c572f031445620554c3d416e1a80291
Mateus Talles <mateustalles@gmail.com>
2020-08-04 19:53:30 -0300

    request managed to log out if token is expired

--- front-end/src/components/admin/OrderCard.js
+++ front-end/src/components/admin/OrderCard.js
@@ -1,26 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
-import '../../styles/OrdersCard.css'
-import formatDateFunc from '../../services/formatDateFunc';
+import '../../styles/AdminOrderCard.css'
 import formatPriceFunc from '../../services/formatPriceFunc';
 
-export default function OrderCard ({ orders }) {
-  const formatedPrice = formatPriceFunc(orders.totalPrice);
-  const formatedDate = formatDateFunc(orders.saleDate);
+export default function OrderCard ({
+  orders: { totalPrice, status, saleId, deliveryNumber, deliveryAddress },
+}) {
+  const formatedPrice = formatPriceFunc(totalPrice);
+  const getStatusColor = () => {
+    if(status === 'Pendente') return { color: 'yellow' }
+    if(status === 'Entregue') return { color: 'green' }
+    return console.error('Status desconhecido.');
+  }
 
   return (
-    <Link
-    to={`/admin/orders/${orders.saleId}`} className="order-card-container"
-    >
-      <div className="order-card-top-content">
-        <div className="order-card-text">{`Pedido ${orders.saleId}`}</div>
-        <div className="order-card-text">{formatedDate}</div>
-      </div>
-      <div className="order-card-bot-content">
-        <div className="order-card-text">{formatedPrice}</div>
-      </div>
-      <div className="order-card-status-content">
-        <span className="order-card-status">{orders.status}</span>
+    <Link to={`/admin/orders/${saleId}`} className="order-card-link">
+      <div className="order-card-container">
+        <div className="order-card-top-content">
+          <div className="order-card-text">{`Pedido ${saleId}`}</div>
+          <div className="order-card-address-container">
+            <span className="order-card-address">{`${deliveryAddress}, ${deliveryNumber}`}</span>
+          </div>
+        </div>
+        <div className="order-card-bot-content">
+          <div className="order-card-text">{formatedPrice}</div>
+          <div className="order-card-status-content">
+            <span className="order-card-status" style={getStatusColor()}>{status}</span>
+          </div>
+        </div>
       </div>
     </Link>
   );
--- front-end/src/pages/admin/Orders.js
+++ front-end/src/pages/admin/Orders.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import OrderCard from '../../components/admin/OrderCard'
 import history  from '../../services/history';
+import '../../styles/AdminOrders.css'
 
 export default function AdminOrders () {
   const [ordersData, setOrdersData] = useState([]);
@@ -11,21 +12,27 @@ export default function AdminOrders () {
     if(!token) return history.push('/login');
 
     const getOrders = async () => {
-      const { data } = await axios({
+      const ordersData = await axios({
         baseURL: 'http://localhost:3001/sales',
         method: 'get',
         headers: { 'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': token }
       })
-      .catch((err) => console.error(err));
-      return setOrdersData(data);
+      .catch(({ response: { status }}) => {
+        return status === 401 && history.push('/login')
+      } );
+      return ordersData && setOrdersData(ordersData.data);
     }
 
     getOrders()
   }, []);
-  console.log(ordersData)
+
   return (
     <div>
-      {ordersData.length !== 0 && ordersData.map((order) => <OrderCard key={order.saleId} orders={order} />)}
+      {ordersData && ordersData.length !== 0 && ordersData.map((order) => (
+        <div className="admin-orders-container">
+          <div className="admin-orders-card"><OrderCard key={order.saleId} orders={order} /></div>
+        </div>
+      ))}
     </div>
   )
 }
