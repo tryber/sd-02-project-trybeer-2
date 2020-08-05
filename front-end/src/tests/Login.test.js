71557dc212a4aa59db0c5273b8cbf606666571ae
eddyeboy27 <lipe0503@gmail.com>
2020-08-05 02:02:35 -0300

    Finalizado Teste do componente Login com 100% de coverage

--- front-end/src/tests/Login.test.js
+++ front-end/src/tests/Login.test.js
@@ -1,8 +1,44 @@
 import React from 'react';
 import { render, fireEvent, wait } from '@testing-library/react';
+import { Router } from 'react-router-dom';
 import history from '../services/history';
 import Provider from '../context/TrybeerContext';
 import Login from '../pages/Login';
+import axios from 'axios';
+
+const usersMock = {
+  data: {
+    email: 'trybe@email.com',
+    name: 'Tryber Tryber',
+    role: 'client',
+    token: '123456',
+  }
+};
+
+const usersAdminMock = {
+  data: {
+    email: 'lipe@lipe.com',
+    name: 'Lipe Tryber',
+    role: 'administrator',
+    token: '123456',
+  }
+};
+
+const dataError = {
+  response: {
+    status: 401,
+    data: {
+      error: {
+        message: 'Error: 401. E-mail not found.'
+      },
+    },
+  },
+};
+
+jest.mock('axios');
+// jest.mock(loginRedirect);
+
+beforeEach(() => localStorage.clear());
 
 describe('Testing Login Page', () => {
   test('Testing if HTML elements appears', () => {
@@ -46,8 +82,64 @@ describe('Testing Login Page', () => {
 
   test('Testing new user button', async () => {
     const { queryByTestId } = render(
-      <Login />
+      <Router history={history}>
+        <Login />
+      </Router>
     );
     
+    const noAccountButton = queryByTestId('no-account-btn');
+    fireEvent.click(noAccountButton);
+    expect(history.location.pathname).toBe('/register');
+  });
+
+  test('Testing login button with client user', async () => {
+    const {queryByTestId} = render(
+      <Login />
+    );
+
+    const emailInput = queryByTestId('email-input');
+    const passInput = queryByTestId('password-input');
+    const signInButton = queryByTestId('signin-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'trybe@email.com' } });
+    fireEvent.change(passInput, { target: { value: '123456' } });
+    axios.mockImplementationOnce(() => Promise.resolve(usersMock));
+    fireEvent.click(signInButton);
+    await wait();
+    expect(history.location.pathname).toBe('/products');
+  });
+
+  test('Testing axios catch in login button', async () => {
+    const { queryByTestId, getByText } = render(
+      <Login />
+    );
+
+    const emailInput = queryByTestId('email-input');
+    const passInput = queryByTestId('password-input');
+    const signInButton = queryByTestId('signin-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'lipe@lipe.com' } });
+    fireEvent.change(passInput, { target: { value: '123456' } });
+    axios.mockImplementationOnce(() => Promise.reject(dataError));
+    fireEvent.click(signInButton);
+    await wait();
+    expect(getByText(/Error: 401. E-mail not found./i, { selector: 'div' })).toBeInTheDocument();
+  });
+
+  test('Testing login button with administrator user', async () => {
+    const {queryByTestId} = render(
+      <Login />
+    );
+
+    const emailInput = queryByTestId('email-input');
+    const passInput = queryByTestId('password-input');
+    const signInButton = queryByTestId('signin-btn');
+
+    fireEvent.change(emailInput, { target: { value: 'lipe@lipe.com' } });
+    fireEvent.change(passInput, { target: { value: '123456' } });
+    axios.mockImplementationOnce(() => Promise.resolve(usersAdminMock));
+    fireEvent.click(signInButton);
+    await wait();
+    expect(history.location.pathname).toBe('/admin/orders');
   });
 });
