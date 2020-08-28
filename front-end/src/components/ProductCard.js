fe13d660e9af342e526e46c9748a09cae0706e23
Mateus Talles <mateustalles@gmail.com>
2020-08-28 14:04:47 -0300

    update

--- front-end/src/components/ProductCard.js
+++ front-end/src/components/ProductCard.js
@@ -5,21 +5,23 @@ import '../styles/ProductCard.css';
 export default function ProductCard ({ index, product: {  id, name, price, urlImage } }) {
   return (
     <div className="product-card-container">
-      <div className="image-container">
-        <img className="product-image" src={urlImage} alt={`Foto do produto: ${name}`} data-testid={`${index}-product-img`}/>
-      </div>
-      <div className="content-container">
-        <div className="product-name-container">
-          <h5 data-testid={`${index}-product-name`}>{name}</h5>
+      <div> </div>
+      <div className="product-content-container">
+        <div className="image-container">
+          <img className="product-image" src={urlImage} alt={`Foto do produto: ${name}`} data-testid={`${index}-product-img`}/>
         </div>
-        <div className="product-price-container">
-          <span data-testid={`${index}-product-price`}>
-            {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price)}
-          </span>
+        <div className="product-name-container">
+          <span className="product-name" data-testid={`${index}-product-name`}>{name}</span>
         </div>
         <div className="add-remove-btns-container">
           <CartAddOrRemoveButtons product={ { id, name, price, urlImage } } index={index} />
         </div>
+        <div> </div>
+      </div>
+      <div className="product-price-container">
+        <span data-testid={`${index}-product-price`}>
+          {new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(price)}
+        </span>
       </div>
     </div>
   )
